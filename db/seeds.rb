c8efbeedd59f0ef1924582c7b173a74be3aa6425
Robin Dickson <robindickson@gmail.com>
2015-10-02 16:15:43 +0200

    Add business and category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cafe = Category.create!(name: 'Cafe')
+restaurant = Category.create!(name: 'Restaurant')
+
+Business.create!(
+  name: 'Cafe Bosanska',
+  address: '22 Main Street',
+  city: 'London',
+  postcode: 'NW6 2EZ',
+  country: 'United Kingdom',
+  phone_number: '01234 555666',
+  web_address: 'www.cafebosanska.com',
+  category: cafe,
+  main_picture_url: '/tmp/cafe_bosanska.jpg',
+)
+
+Business.create!(
+  name: 'Fruhstuck',
+  address: '8 Neukolln Avenue',
+  city: 'London',
+  postcode: 'SW2 5MN',
+  country: 'United Kingdom',
+  phone_number: '09876 773321',
+  web_address: 'www.fruhstuckisthebest.com',
+  category: cafe,
+  main_picture_url: '/tmp/fruhstuck.jpg',
+)
+
+Business.create!(
+  name: "Suh's",
+  address: '234 36th Street',
+  city: 'Chicago',
+  postcode: '60607',
+  country: 'United States',
+  phone_number: '312 111 222',
+  web_address: 'www.suhsrestaurant.com',
+  category: restaurant,
+  main_picture_url: '/tmp/suhs.jpg',
+)
