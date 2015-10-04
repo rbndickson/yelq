5c5284a32486bd28472b80803401dd9157887ddf
Robin Dickson <robindickson@gmail.com>
2015-10-04 15:41:56 +0200

    Add sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 class SessionsController < ApplicationController
 
   def new
-    
+    redirect_to home_path if signed_in?
   end
 
   def create
-
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path
+      flash[:success] = 'You are now signed in.'
+    else
+      redirect_to login_path
+      flash[:danger] = 'Error in signing in.'
+    end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+    flash[:info] = 'You have now signed out.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
+  root 'businesses#index'
+  get 'home', to: 'businesses#index'
+
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   resources :businesses, only: [:show, :index]
   resources :users, only: [:create]
