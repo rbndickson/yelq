71eb78981f46e6f04a245315f80d2dd78f370da7
Robin Dickson <robindickson@gmail.com>
2015-10-10 10:05:45 +0100

    Add user edit and update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,13 @@ class ApplicationController < ActionController::Base
   def signed_in?
     !!current_user
   end
+
+  def require_user
+	  access_denied unless logged_in?
+  end
+
+  def access_denied
+    flash[:error] = "Sorry you do not have access"
+    redirect_to root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-  end
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -12,15 +9,36 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to login_path
-      flash[:success] = "You have been registered, please sign in."
+      flash[:success] = "You have been registered, please log in."
     else
       render :new
     end
+  end
+
+  def edit
+    access_denied if @user != current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your profile was updated."
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def show
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
