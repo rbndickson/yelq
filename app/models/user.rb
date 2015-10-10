34c82532cea30c9391d4f84d567d5d57c12d7fbd
Robin Dickson <robindickson@gmail.com>
2015-10-10 10:58:46 +0100

    Change user model to validate password length on update as well as create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name, :email, :password_digest
   validates_uniqueness_of :email
-  validates_length_of :password, minimum: 8, on: :create
+  validates_length_of :password, minimum: 8
 
   def last_name_intital
     last_name[0]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,7 @@ describe User do
   it { is_expected.to validate_uniqueness_of(:email) }
 
   it do
-    is_expected.to validate_length_of(:password).
-    is_at_least(8).
-    on(:create)
+    is_expected.to validate_length_of(:password).is_at_least(8)
   end
 
   describe '#last_name_intital' do
