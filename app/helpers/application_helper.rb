6959917004b642d6ce9ea5be876108df634c9e70
Robin Dickson <robindickson@gmail.com>
2015-10-11 23:49:57 +0100

    Add Gravatar and Robohash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,9 @@ module ApplicationHelper
   def star_options
     [5, 4, 3, 2, 1].collect { |number| [pluralize(number, 'star'), number] }
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "https://robohash.org/#{gravatar_id}?gravatar=hashed&size=200x200&bgset=bg1"
+  end
 end
